4ffbba4f57c8ca37b547b644d6523b2db25c2236
CL16gtgh <ryang359@gatech.edu>
2024-03-27 19:10:30 -0400

    pending solutions. pls dont judge

--- HyTech_CAN/HyTech_CAN.h
+++ HyTech_CAN/HyTech_CAN.h
@@ -38,6 +38,12 @@
 #include "SAB_readings_front.h"
 #include "SAB_readings_rear.h"
 #include "SAB_readings_gps.h"
+
+#include "SAB_CB.h"
+#include "SAB_thermistors_1.h"
+#include "SAB_thermistors_2.h"
+#include "TCU_status.h"
+
 #include "GPS_high_precision.h"
 #include "GPS_lat_long.h"
 #include "GPS_other.h"
@@ -45,4 +51,6 @@
 #include "EM_measurement.h"
 #include "IMU_accelerometer.h"
 #include "IMU_gryoscope.h"
+
+#
 #endif
